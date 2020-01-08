5c75963b68a21afe3b8f89306e3b2a19f0f6f815
kk2491 <kishorkumar2491@gmail.com>
2020-01-08 12:12:05 +0530

    mahalanobis example - wip - 3

---
+++ mahalanobis_distance_toy_expt/maha_3.py
@@ -0,0 +1,6 @@
+from scipy.spatial import distance
+from sklearn.cluster import KMeans
+
+if __name__ == "__main__":
+
+	data = { (5,3), (10,15), (15,12), (24,10), (30,45), (85,70), (71,80), (60,78), (55,52), (80,91) }
